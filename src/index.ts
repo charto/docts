2673605d429e38d025daecaca07d37414b31f544
jjrv <juha@busfaster.com>
2018-02-28 20:59:54 +0200

    Clean up project structure and bump version.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// This file is part of docts, copyright (c) 2016 BusFaster Ltd.
+// Released under the MIT license, see LICENSE.
+
+export { Section, Markdown } from './Markdown';
+export { DocBuilder } from './DocBuilder';
+export { patchReadme } from './Patcher';
